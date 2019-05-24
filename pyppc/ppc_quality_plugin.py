2026bc1850dc311dbf99b1f62d85614b0b5fadfd
Anders Henja <anders@baltrad.eu>
2019-05-24 15:33:44 +0200

    Added handling of latest melting layer

--- pyppc/ppc_quality_plugin.py
+++ pyppc/ppc_quality_plugin.py
@@ -50,6 +50,13 @@ try:
 except:
   logger.exception("Failed to load options")
 
+nodomdb=False
+try:
+  import rave_dom_db
+except:
+  nodomdb=True
+
+
 # The baltrad-ppc quality plugin
 #
 class ppc_quality_plugin(rave_quality_plugin):
@@ -62,16 +69,20 @@ class ppc_quality_plugin(rave_quality_plugin):
   def get_options(self, polarobj):
     odim_source.CheckSource(polarobj)
     S = odim_source.ODIM_Source(polarobj.source)
+    nod = None
+    if S is not None and S.nod is not None:
+      nod = S.nod
+
     if self._options:
       if self._options.exists(S.nod):
         #logger.info("Using %s ppc radar options"%S.nod)
-        return self._options.getRadarOptions(S.nod)
+        return nod, self._options.getRadarOptions(S.nod)
       elif self._options.exists("default"):
         #logger.info("Using default ppc radar options")
-        return self._options.getRadarOptions("default")
+        return nod, self._options.getRadarOptions("default")
     if S is not None and S.nod is not None:
       logger.info("Check configuration! Using backup default radar option for %s"%S.nod)
-    return _ppcradaroptions.new()
+    return nod, _ppcradaroptions.new()
   
   ##
   # @return a list containing the string se.baltrad.ppc.residual_clutter_mask
@@ -91,7 +102,15 @@ class ppc_quality_plugin(rave_quality_plugin):
           if reprocess_quality_flag == False and obj.findQualityFieldByHowTask("se.baltrad.ppc.residual_clutter_mask") != None:
             return obj
           processor = _pdpprocessor.new()
-          processor.options = self.get_options(obj)
+          nod, processor.options = self.get_options(obj)
+          if not nodomdb and nod is not None:
+            try:
+              db = rave_dom_db.create_db_from_conf()
+              latest=db.get_latest_melting_layer(nod)
+              if latest is not None and latest.bottom is not None:
+                processor.meltingLayerBottomHeight = latest.bottom
+            except Exception as e:
+              logger.error("Failed to determine melting layer bottom height: "%e.__str__())
           processor.options.requestedFields = processor.options.requestedFields | _ppcradaroptions.P_DBZH_CORR | _ppcradaroptions.P_ATT_DBZH_CORR | _ppcradaroptions.Q_RESIDUAL_CLUTTER_MASK
           result = processor.process(obj)
           obj.addOrReplaceQualityField(result.getQualityFieldByHowTask("se.baltrad.ppc.residual_clutter_mask"))
@@ -101,13 +120,25 @@ class ppc_quality_plugin(rave_quality_plugin):
             obj.addParameter(f)
           
         elif _polarvolume.isPolarVolume(obj):
-          options = self.get_options(obj)
+          nod, options = self.get_options(obj)
+          meltingLayer = None
+          if not nodomdb and nod is not None:
+            try:
+              db = rave_dom_db.create_db_from_conf()
+              latest=db.get_latest_melting_layer(nod)
+              if latest is not None and latest.bottom is not None:
+                meltingLayer = latest.bottom
+            except Exception as e:
+              logger.error("Failed to determine melting layer bottom height: "%e.__str__())
+          
           for i in range(obj.getNumberOfScans()):
             scan = obj.getScan(i)
             if reprocess_quality_flag == False and scan.findQualityFieldByHowTask("se.baltrad.ppc.residual_clutter_mask") != None:
               continue
             processor = _pdpprocessor.new()
             processor.options = options
+            if meltingLayer is not None:
+              processor.meltingLayerBottomHeight = meltingLayer
             processor.options.requestedFields = processor.options.requestedFields | _ppcradaroptions.P_DBZH_CORR | _ppcradaroptions.P_ATT_DBZH_CORR | _ppcradaroptions.Q_RESIDUAL_CLUTTER_MASK
             result = processor.process(scan)
             scan.addOrReplaceQualityField(result.getQualityFieldByHowTask("se.baltrad.ppc.residual_clutter_mask"))
